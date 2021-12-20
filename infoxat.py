5ae870a918a0b9395b0f09eae2846c8c7f5ac729
jose89fcb <60312427+jose89fcb@users.noreply.github.com>
2021-12-20 15:58:44 +0100

    modificando algunas cosas...

--- infoxat.py
+++ infoxat.py
@@ -5,6 +5,70 @@ import json
 from os import system
 import os
 
+import discord
+from discord.ext import commands
+import requests
+import json
+from os import system
+import os
+
+
+
+bot = commands.Bot(command_prefix='!', description="ayuda bot") #Comando
+bot.remove_command("help") # Borra el comando por defecto !help
+
+
+@bot.command()
+async def xat(ctx, *, Nombrexat):
+    response = requests.get(f"https://xat.com/web_gear/chat/roomid.php?d={Nombrexat}")
+    xat = response.json()['a']
+    id = response.json()['id']
+    descripcion = response.json()['d']
+
+    infoxat = xat
+
+    separador = ";="
+   
+    separado = infoxat.split(separador)    
+
+    url_imagen = format(separado[0])
+    nombre_local_imagen = "fondoxat.png"
+    
+    
+    imagen = requests.get(url_imagen).content
+
+
+    
+   
+    with open(nombre_local_imagen, 'wb') as handler:
+        handler.write(imagen)
+
+
+   
+    
+        
+        
+    
+    usuario =  ctx.message.author
+  
+    embed = discord.Embed(title="", description="Id de xat: " f"{id}""\n\ndescripción: " f"{descripcion}" "\n\nidioma xat: {}".format(separado[3]) + "\n\nLink radio: {}".format(separado[4]).replace("/;"," ").replace(";"," "), color=discord.Colour.random()) 
+        
+    embed.set_footer(text="Programado Por jose89fcb" + "\n\nComando ejecutado Por🡺 " + (str(usuario)), icon_url="https://i.imgur.com/tYMmIH1.png")
+    embed.set_author(name="INFO xat") 
+   
+
+    embed.set_image(url="attachment://fondoxat.png")
+  
+   
+        
+    await ctx.send(embed=embed, file=discord.File('fondoxat.png'))
+    os.remove("fondoxat.png")
+      
+
+
+
+bot.run('ODQ5ODYxNjQ2MjI1NTA2MzU0.YLhVVg.8wMoKJ_vuaNGKo_ub5JSDc2YqaI')
+
 
 
 bot = commands.Bot(command_prefix='!', description="ayuda bot") #Comando
@@ -58,3 +122,67 @@ async def xat(ctx, *, Nombrexat):
 
 
 bot.run('AQUI EL TOKEN') #Deberas de crear tu propio bot e TOKEN en el siguiente enlace: https://discord.com/developers/applications y añadirlo en AQUI EL TOKEN
+import discord
+from discord.ext import commands
+import requests
+import json
+from os import system
+import os
+
+
+
+bot = commands.Bot(command_prefix='!', description="ayuda bot") #Comando
+bot.remove_command("help") # Borra el comando por defecto !help
+
+
+@bot.command()
+async def xat(ctx, *, Nombrexat):
+    response = requests.get(f"https://xat.com/web_gear/chat/roomid.php?d={Nombrexat}")
+    xat = response.json()['a']
+    id = response.json()['id']
+    descripcion = response.json()['d']
+
+    infoxat = xat
+
+    separador = ";="
+   
+    separado = infoxat.split(separador)    
+
+    url_imagen = format(separado[0])
+    nombre_local_imagen = "fondoxat.png"
+    
+    
+    imagen = requests.get(url_imagen).content
+
+
+    
+   
+    with open(nombre_local_imagen, 'wb') as handler:
+        handler.write(imagen)
+
+
+   
+    
+        
+        
+    
+    usuario =  ctx.message.author
+  
+    embed = discord.Embed(title="", description="Id de xat: " f"{id}""\n\ndescripción: " f"{descripcion}" "\n\nidioma xat: {}".format(separado[3]) + "\n\nLink radio: {}".format(separado[4]).replace("/;"," ").replace(";"," "), color=discord.Colour.random()) 
+        
+    embed.set_footer(text="Programado Por jose89fcb" + "\n\nComando ejecutado Por🡺 " + (str(usuario)), icon_url="https://i.imgur.com/tYMmIH1.png")
+    embed.set_author(name="INFO xat") 
+   
+
+    embed.set_image(url="attachment://fondoxat.png")
+  
+   
+        
+    await ctx.send(embed=embed, file=discord.File('fondoxat.png'))
+    os.remove("fondoxat.png")
+      
+
+
+
+bot.run('')#Deberas de crear tu propio bot e TOKEN en el siguiente enlace: https://discord.com/developers/applications y añadirlo en AQUI EL TOKEN
+
